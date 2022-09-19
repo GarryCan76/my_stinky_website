8927c5990adf2b83c9a51c07cad227396aa67955
GarryCan76 <95485005+GarryCan76@users.noreply.github.com>
2022-09-19 17:39:35 +0200

    oki

---
+++ script.js
@@ -0,0 +1,31 @@
+let color_red = 255;
+let color_green = 0;
+let color_blue = 0;
+let state = 0;
+setInterval(color_anim, 10)
+
+function color_anim() {
+    if (color_red >= 255 && color_green <= 0) {
+        state = 1;
+    }
+    if (color_green >= 255 && color_blue <= 0) {
+        state = 2;
+    }
+    if (color_blue >= 255 && color_red <= 0) {
+        state = 3;
+    }
+    if (state == 1){
+        color_red --;
+        color_green ++;
+    }
+    if (state == 2){
+        color_green --;
+        color_blue ++;
+    }
+    if (state == 3){
+        color_blue --;
+        color_red ++;
+    }
+    console.log(color_red)
+    document.getElementById("rgb_slider").style.backgroundImage = "linear-gradient(0deg,rgba("+color_red+", "+color_green+", "+color_blue+",100),rgba("+color_green+", "+color_blue+", "+color_red+",100))"
+}
